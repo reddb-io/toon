23f2efedcda8cce95358fe7495287472ec1c151b
filipeforattini <filipeforattini1@gmail.com>
2026-08-06 09:33:29 -0300

    build(toon): sync generated index.js
    Refs #209

--- packages/toon/dist/index.js
+++ packages/toon/dist/index.js
@@ -6,8 +6,8 @@
 export { VERSION } from './version.js';
 export { ToonError, ToonlCursorInvalidationError, ToonlError } from './errors.js';
 export { DEFAULT_INDENT, DEFAULT_MAX_DEPTH, detectTruncation, parse, parseDocument, serialize, } from './toon.js';
-export { parse as decode, serialize as encode } from './toon.js';
+export { encode } from './encode/serialize.js';
 export { decodeStream, decodeStreamSync } from './decode/stream.js';
-export { buildValueFromEvents, decodeValue } from './decode/build.js';
+export { buildValueFromEvents, decodeValue, decodeValue as decode } from './decode/build.js';
 export { appendSummaryField, projectFields } from './helpers.js';
 export { JsonlToToonl, ToonlDecodeStream, ToonlEncodeStream, ToonlToJsonl, ToonlEncoder, ToonlReader, closeTransform, closeTransformInterleaved, decodeLines, encodeLines, encodeRecords, jsonToToon, parseRecords, parseStream, recordTransform, toonToJson, } from './toonl.js';
